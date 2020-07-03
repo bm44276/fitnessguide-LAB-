b63501dfaebabe915bd1e40b3e824918582d8a2b
Jon Morina 23 <64468973+JonMorina@users.noreply.github.com>
2020-07-03 14:56:55 +0200

    Added footer.php

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+<style>
+    footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 1px gray solid;
+    padding: 10px;
+    border-top: 4px #274957 solid;
+}
+
+footer img{
+    width: 100px;
+    margin-right: 10px;
+}
+
+footer h3{
+    font-size: 20px;
+    letter-spacing: 1px;
+}
+</style>
+
+        <img src="images/Component 12 – 1@2x.png" alt="">
+        <h3>&copy; YourFitnessGuide.com</h3>
+    
