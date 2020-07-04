b562eaf58fb0e027cb3cf9ba5348d3cdece308e6
Jon Morina 23 <64468973+JonMorina@users.noreply.github.com>
2020-07-04 17:58:57 +0200

    Update thankYouPage.php

--- thankYouPage.php
+++ thankYouPage.php
@@ -4,9 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+        <style>
+     *{
+         padding:0;
+         margin: 0;
+     }
+
+     body{
+            font-family: sans-serif;
+            background-color: #4996da;
+            text-align: center;
+            background-image: url(images/Component2.png);
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 1000px;
+        }
+
+        h1{
+            font-size: 40px;
+            color:white;
+            padding-top:400px;
+            margin-right: 500px;
+            padding-bottom: 100px;
+        }
+
+        a{
+            width: 70px;
+            font-size: 22px;
+            letter-spacing: 1px;
+            font-weight: 700;
+            outline: none;
+            cursor: pointer;
+            border: 2px #41819A solid;
+            padding: 5px;
+            text-decoration: none;
+            color: white; 
+        }
+ a:hover{
+            border-color: #356477;
+            background-color: #356477;
+            color: white;
+        }
+
+
+    </style>
 </head>
 <body>
-    <h1>Your password was creates succefully</h1>
-    
+    <h1>Your account was created succefully</h1>
+    <a href="logInPage.php">Log In</a>
 </body>
 </html>
